fa09410cab11a206ae546184e724935b5f452f14
Andre Ferreira <andrehrf@gmail.com>
2025-06-04 18:15:18 -0300

    remove overlay loading

--- apps/web/src/theme-gamedevbr/views/PageHome.vue
+++ apps/web/src/theme-gamedevbr/views/PageHome.vue
@@ -963,35 +963,9 @@ watch(() => settings.value['blog.cover'], () => {
     opacity: 1;
 }
 
-.lazy-image::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: linear-gradient(90deg, #f0f0f0 25%, #e0e0e0 50%, #f0f0f0 75%);
-    background-size: 200% 100%;
-    animation: loading 1.5s infinite;
-    z-index: 1;
-}
-
-.lazy-image.loaded::before {
-    display: none;
-}
-
 .lazy-image.error {
     opacity: 0.5;
     filter: grayscale(100%);
-}
-
-@keyframes loading {
-    0% {
-        background-position: 200% 0;
-    }
-    100% {
-        background-position: -200% 0;
-    }
 }
 </style>
 
