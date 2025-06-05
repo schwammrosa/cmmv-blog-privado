6731deb81b3e8a6157b140789e03ab5d1cdf6aba
Kleberson Romero <klebersondsromero@gmail.com>
2025-06-04 21:00:14 -0300

    feat: Fix Menu icon colors.

--- apps/web/src/theme-cupomnahora/templates/TemplateDefault.vue
+++ apps/web/src/theme-cupomnahora/templates/TemplateDefault.vue
@@ -140,12 +140,12 @@
 
                     <!-- Mobile Menu Button -->
                     <div class="md:hidden flex items-center space-x-3">
-                        <button @click="openSearchModal" class="text-gray-700 hover:text-indigo-600" title="Search" aria-label="Search">
+                        <button @click="openSearchModal" class="text-white hover:text-indigo-200" title="Search" aria-label="Search">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                             </svg>
                         </button>
-                        <button @click="mobileMenuOpen = !mobileMenuOpen" class="text-gray-700" title="Navbar" aria-label="Navbar">
+                        <button @click="mobileMenuOpen = !mobileMenuOpen" class="text-white" title="Navbar" aria-label="Navbar">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path v-if="mobileMenuOpen" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                                 <path v-else stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
