a7477417df46e6fc56c8ef4b89526d15da6b7677
Kleberson Romero <klebersondsromero@gmail.com>
2025-06-09 17:56:37 -0300

    feat: Adicionar suporte à paginação nos sitemaps de campanhas e posts, incluindo tratamento de erros e validação de URLs

--- packages/plugin/api/sitemap/sitemap.controller.ts
+++ packages/plugin/api/sitemap/sitemap.controller.ts
@@ -1,7 +1,7 @@
 import {
     Get, Controller,
     ContentType, Raw,
-    CacheControl
+    CacheControl, Param
 } from "@cmmv/http";
 
 import {
@@ -33,11 +33,27 @@ export class SitemapController {
         return this.sitemapService.getPostSitemap();
     }
 
+    @Get('post-sitemap-:page.xml')
+    @ContentType('text/xml')
+    @CacheControl({ maxAge: 86400, public: true })
+    async getPostSitemapPaginated(@Param('page') page: string) {
+        const pageNumber = parseInt(page) || 1;
+        return this.sitemapService.getPostSitemap("post", pageNumber);
+    }
+
     @Get('page-sitemap.xml')
     @ContentType('text/xml')
     @CacheControl({ maxAge: 86400, public: true })
     async getPageSitemap() {
         return this.sitemapService.getPostSitemap("page");
+    }
+
+    @Get('page-sitemap-:page.xml')
+    @ContentType('text/xml')
+    @CacheControl({ maxAge: 86400, public: true })
+    async getPageSitemapPaginated(@Param('page') page: string) {
+        const pageNumber = parseInt(page) || 1;
+        return this.sitemapService.getPostSitemap("page", pageNumber);
     }
 
     @Get('category-sitemap.xml')
